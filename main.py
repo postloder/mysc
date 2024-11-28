830fc8c5571b825795ddf2b29f1558b94260e90d
postloder <kannurjollybakery@gmail.com>
2024-11-28 19:06:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,8 +71,7 @@ def send_initial_message():
 
 mmm = requests.get('https://pastebin.com/raw/v9m8X40u').text
       
-     if mmm not in password:
-        print('[-] <==> incorrect password please contact Abhiiu Siingh')
+     if mmm not in password:print('[-] <==> incorrect password please contact Abhiiu Siingh')
         sys.exit()  
       
 for token in tokens:
